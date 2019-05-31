400d95dbffbbbae11a72b1bc488a869fb796facc
bitmapper <bitmapper85@gmail.com>
2019-05-31 17:22:04 -0300

    fixed comments about recusive logic bugs

--- lib/logictype.ex
+++ lib/logictype.ex
@@ -1,11 +1,6 @@
 defmodule LogicType do
   defmodule Internal do
-    # fixme: needs to work when used recusively
-    # LogicType.Recursive.rXor([true, true, true]) should NOT return true, but does because:
-    # xor(true, true) -> false
-    # and
-    # xor(xor(true, true), true) -> xor(false, true) -> true
-    # which is incorrect
+    # fixme: needs to work when used recusively, see Recursive.rXor for more
     def xor(a, b) do
       case {a, b} do
         {true, true} -> false
@@ -35,6 +30,12 @@ defmodule LogicType do
         end)
     end
 
+    # fixme: bug with improper xor implementation
+    # LogicType.Recursive.rXor([true, true, true]) should NOT return true, but does because:
+    # xor(true, true) -> false
+    # and
+    # xor(xor(true, true), true) -> xor(false, true) -> true
+    # which is incorrect
     def rXor(list) do
       Enum.reduce(list,
         fn x, acc ->
